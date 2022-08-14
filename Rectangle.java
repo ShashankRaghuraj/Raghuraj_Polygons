3a45bd693ff27dca86fff2197b94422ccf88e696
ShashankRaghuraj <80557791+ShashankRaghuraj@users.noreply.github.com>
2022-08-14 13:08:06 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,39 @@
+//Shashank Raghuraj
+//Due 10/23/2020
+//Task: Create another child class called Rectangle. Find the area of the rectangle. Determine if the rectangle is a square based on side lengths
+
+public class Rectangle extends Polygons
+{
+  //Creates a boolean called is Square and sets it to false
+  private boolean isSquare = false;
+  private String shape = "rectangle";
+  //Constructor method that takes in both a width and height
+  public Rectangle(double w, double h)
+	{
+    //Calls the parent constructor which then it sets the width and the height in the parent constructor
+		super(w, h);
+    //Determines whether width and height are same. If so, then isSquare becomes true
+    if(w == h)
+    {
+      isSquare = true;
+      shape = "square";
+    }
+	}
+  //returns isSquare(true or false)
+  public boolean getIsSquare()
+  {
+    return isSquare;
+  }
+  //Returns area(a double)
+  public double getArea()
+	{
+		return getWidth()*getHeight();
+	}
+  //Displays all info
+  public void displayInfo()
+	{
+		System.out.println("\n\nSHAPE INFO:");
+		System.out.println("The figure is a " + shape + ".");
+		super.displayInfo();
+	}	
+}
